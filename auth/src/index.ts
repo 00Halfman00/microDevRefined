108b2e015017e2f2403922c760e81f7db6147180
oscar sanchez <00oscarsanchez00@gmail.com>
2025-06-15 15:26:47 -0500

    setup mongo db to create user that works with typescript and added startServers function in index.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import { json } from 'body-parser';
 import 'express-async-errors';
+import mongoose from 'mongoose';
+// testing db
+import { User } from './models/user';
+
 // route handlers
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -22,6 +26,27 @@ app.use(NotFoundRouter);
 // use error handler
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('auth SERVER listening on port 3000');
-});
+const startServers = async () => {
+  try {
+    const db = await mongoose.connect(
+      'mongodb://auth-mongo-clusterip-srv:27017/auth-db'
+    );
+    console.log('auth-db SERVER listening on port 27017');
+    const user = User.build({
+      email: 'one@one.com',
+      password: 'onetwothree',
+    });
+
+    console.log('user: ', user);
+
+    app.listen(3000, () => {
+      console.log('auth SERVER listening on port 3000');
+    });
+  } catch (err) {
+    console.error('Error starting auth and auth-db SERVERS', err);
+    // Optionally, exit the process if a critical startup component fails
+    process.exit(1);
+  }
+};
+
+startServers();
---
+++ auth/src/models/user.ts
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+UserSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', UserSchema);
+
+const user = User.build({
+  email: 'one@one.com',
+  password: 'onetwothree',
+});
+
+export { User };
