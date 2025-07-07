27d575a3236287ef46bddfee8e54ad470642e008
oscar sanchez <00oscarsanchez00@gmail.com>
2025-07-07 17:21:44 -0500

    setup current user and authorization middlewares and a not authorized error handler

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,18 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import jwt from 'jsonwebtoken';
+import { CurrentUser } from '../middlewares/current-user';
 
-router.get('/api/users/currentuser', async (req, res) => {
-  console.log(req.session);
-  if (!req.session?.jwt) {
-    res.send({ currentUser: null });
-  }
-  try {
-    const payload = jwt.verify(req.session?.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (err) {
-    res.send({ currentUser: null });
-  }
+router.get('/api/users/currentuser', CurrentUser, async (req, res) => {
+  res.send({ CurrentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
